ca7cd008789cacbcb5e9363d2328ba53c412245d
JTay093 <jtaing01@northshore.edu>
2026-02-27 11:34:53 -0500

    Added CSS file, removed and edited filez

--- backend/server/server.js
+++ backend/server/server.js
@@ -10,6 +10,8 @@ const editUser = require('./routes/userEditUser')
 const deleteUser = require('./routes/userDeleteAll')
 const addWatchlistItem = require('./routes/watchlist/addWatchlist')
 const fetchWatchlistItem = require('./routes/watchlist/fetchWatchlist')
+const commentRoutes = require('./routes/Comments');
+const voteRoutes = require('./routes/vote');
 require('dotenv').config();
 const SERVER_PORT = 8081
 
@@ -24,6 +26,8 @@ app.use('/user', editUser)
 app.use('/user', deleteUser)
 app.use('/watchlist', addWatchlistItem)
 app.use('/watchlist', fetchWatchlistItem)
+app.use('/comments', commentRoutes);
+app.use('/vote', voteRoutes);
 app.listen(SERVER_PORT, (req, res) => {
     console.log(`The backend service is running on port ${SERVER_PORT} and waiting for requests.`);
 })
