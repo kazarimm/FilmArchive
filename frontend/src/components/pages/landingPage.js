6337822ef2eb523a7b752598c0493c8dc2229d7d
Marcos Salazar <salazarjmarcos@gmail.com>
2026-04-15 12:16:26 -0400

    Changed landing page Card

--- frontend/src/components/pages/landingPage.js
+++ frontend/src/components/pages/landingPage.js
@@ -28,12 +28,12 @@ const Landingpage = () => {
         {/* Card */}
         <Card style={{ width: "30rem" }} className="mx-2 my-2">
           <Card.Body>
-            <Card.Title>Professor Brockenbrough's User Skeleton App</Card.Title>
+            <Card.Title>Welcome to Film Archive (Placeholder)</Card.Title>
             <Card.Subtitle className="mb-2 text-muted">
-              A starting point for an application.
+              
             </Card.Subtitle>
             <Card.Text>
-              If you see this with a black background and white text, Tailwind is working
+             
             </Card.Text>
             <Card.Link href="/signup">Sign Up</Card.Link>
             <Card.Link href="/login">Login</Card.Link>
